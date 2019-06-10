dba67db71065bb1d185bfcdcdf868eb5a5e7b3ca
footballitis.com <sshk-pc@sk.local>
2019-06-10 23:20:37 +0900

    fix court

--- app/models/court.rb
+++ app/models/court.rb
@@ -1,3 +1,7 @@
 class Court < ApplicationRecord
     belongs_to :user
+
+    validates :store_name, presence: true
+    VALID_STORE_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :store_email, length: { maximum: 255 }, format:      y{ with: VALID_STORE_EMAIL_REGEX }
 end
