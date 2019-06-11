e269088c6380de9c7473a6b47061d10610325b47
footballitis.com <sshk-pc@sk.local>
2019-06-11 23:42:32 +0900

    fix model and view

--- app/models/court.rb
+++ app/models/court.rb
@@ -2,6 +2,11 @@ class Court < ApplicationRecord
     belongs_to :user
 
     validates :store_name, presence: true
-    VALID_STORE_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :store_email, length: { maximum: 255 }, format:      y{ with: VALID_STORE_EMAIL_REGEX }
+
+    validates :store_email, length: { maximum: 255 }
+
+    VALID_TEL_REGEX = /\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?
+                      \d{4})[-)]?)|\d{1,4}\-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/
+    validates :tel, format: { with: VALID_TEL_REGEX }
+    # 携帯ハイフンありにも対応したほうがいい？
 end
