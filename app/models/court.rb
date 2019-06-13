eba8f54133290fcf08600b9ec751aced4acd0566
footballitis.com <sshk-pc@sk.local>
2019-06-13 20:33:46 +0900

    add model regex

--- app/models/court.rb
+++ app/models/court.rb
@@ -3,10 +3,12 @@ class Court < ApplicationRecord
 
     validates :store_name, presence: true
 
-    validates :store_email, length: { maximum: 255 }
+    VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates :store_email, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+                                allow_blank: true
 
-    VALID_TEL_REGEX = /\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?
-                      \d{4})[-)]?)|\d{1,4}\-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/
-    validates :tel, format: { with: VALID_TEL_REGEX }
-    # 携帯ハイフンありにも対応したほうがいい？
+    VALID_TEL_REGEX = /\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?\d{4})[-)]?)|\d{1,4}\-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/
+    validates :tel, format: { with: VALID_TEL_REGEX }, allow_blank: true
+
+
 end
